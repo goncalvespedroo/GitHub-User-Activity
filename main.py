f544fb4dd7f8c72271229fc1ced929752302bc05
goncalvespedroo <contato.pedrojson@gmail.com>
2026-04-24 18:20:16 -0300

    feat: transform script into a CLI tool using setuptools

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import requests
+import sys
 
-userName = "goncalvespedroo"
-url = f"https://api.github.com/users/{userName}/events"
-response = requests.get(url)
+def fetch_githubActivity (username):
+    url = f"https://api.github.com/users/{username}/events"
+    response = requests.get(url)
 
-events = response.json()
+    events = response.json()
 
-for event in events:
-    type = event['type']
-    repo = event['repo']['name']
+    for event in events:
+        type = event['type']
+        repo = event['repo']['name']
 
-    print(f"Name: {type} | Repository: {repo}")
+        print(f"Name: {type} | Repository: {repo}")
+
+def main():
+    if len(sys.argv) < 2:
+        print("Error: Please provide a username.")
+        print("Usage: githubactivity <username>")
+        sys.exit(1) 
+    username = sys.argv[1]
+    fetch_githubActivity(username)
+
+if __name__ == "__main__":
+            main()
